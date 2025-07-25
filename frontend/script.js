b8cb0df78f5b5c7c9d1148ee8380b86df548f9c9
AidaNightcore <dellfin4@gmail.com>
2025-07-25 05:54:42 +0300

    language options

--- frontend/script.js
+++ frontend/script.js
@@ -36,3 +36,42 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+function switchLanguage(lang) {
+  document.querySelectorAll("[lang]").forEach((el) => {
+    el.style.display = "none";
+  });
+
+  let elements = document.querySelectorAll('[lang="' + lang + '"]');
+  if (elements.length === 0) {
+    lang = "en";
+    elements = document.querySelectorAll('[lang="en"]');
+  }
+  elements.forEach((el) => {
+    el.style.display = "";
+  });
+
+  document.querySelectorAll(".lang-btn").forEach((btn) => {
+    btn.classList.toggle("active", btn.dataset.lang === lang);
+  });
+
+  localStorage.setItem("preferredLang", lang);
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  let lang = localStorage.getItem("preferredLang");
+
+  if (!lang) {
+    const browserLang = navigator.language || navigator.userLanguage;
+    lang = browserLang.split("-")[0];
+  }
+
+  switchLanguage(lang);
+});
+
+document.querySelectorAll(".lang-btn").forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const selectedLang = this.dataset.lang;
+    switchLanguage(selectedLang);
+  });
+});
